a05a26749f041cf84112eb3b894aef3af03449d3
Anastasia <120001795+fgnpl@users.noreply.github.com>
2026-07-09 15:01:29 +0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,26 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hashMap; 
+
+        for (const string& s : strs) {
+            string sorted_s = s;
+            sort(sorted_s.begin(), sorted_s.end());
+            hashMap[sorted_s].push_back(s);
+        }
+
+        vector<vector<string>> result; 
+        for (auto& pair : hashMap) {
+            result.push_back(std::move(pair.second));
+        }
+
+        return result;
+
+    }
+};
